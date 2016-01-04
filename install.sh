95a0d5f2e5ccbd225aee2229df9bc508af3c5e6e
Achilleas Tsoumitas <knorcedger@gmail.com>
2016-01-04 14:04:15 +0000

    Enable main universe restricted multiverse repos

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo echo Your are about to experience magic!
 sudo add-apt-repository -y ppa:'webupd8team/atom' &> /dev/null
 sudo wget -q https://dl-ssl.google.com/linux/linux_signing_key.pub -O- | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
 sudo apt-get update
 
